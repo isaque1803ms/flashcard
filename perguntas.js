e95709a3cf65d02599b2c4a3a1fbccbc09629c3f
isaque1803ms <00001090623537SP@al.educacao.sp.gov.br>
2024-11-29 20:23:33 -0300

    flashcard

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Musica',
+    'Bateria é um instrumento de corda?',
+    'Bateria é um intrumento de percurssao'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital do Brasil?',
+    'A capital do Brasil é Brasilia'
+)
+
+criaCartao(
+    'Biologia',
+    'Aranha é um inseto?',
+    'A aranha é um aracnideo'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz tchau em Inglês?',
+    'Tchau em ingles é GOODBYE'
+)
